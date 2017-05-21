cdeb947fc51ed948a2d876677c821c1cf45a7fe3
Chris Ha <chunghha@users.noreply.github.com>
2017-05-21 02:25:26 -0500

    feat(logging): kotlin-logging

--- src/main/kotlin/myspark/App.kt
+++ src/main/kotlin/myspark/App.kt
@@ -1,17 +1,28 @@
 package myspark
 
-import spark.Spark.*
+import mu.KotlinLogging
+import org.slf4j.Logger
+import spark.Request
+import spark.Spark.get
+
+private val LOGGER = KotlinLogging.logger {}
 
 fun main(args: Array<String>) {
-    get("/") { req, res -> hello() }
-    get("/hello") { req, res -> hello() }
-    get("/world") { req, res -> world() }
+    get("/") { req, res -> hello(req) }
+    get("/hello") { req, res -> hello(req) }
+    get("/world") { req, res -> world(req) }
 }
 
-fun hello(): String {
+fun hello(req: Request): String {
+    logPath(req)
+
     return "Hello World"
 }
 
-fun world(): String {
+fun world(req: Request): String {
+    logPath(req)
+
     return "Rock your World"
 }
+
+private fun logPath(req: Request) = LOGGER.info("Call to {}", req.pathInfo())
